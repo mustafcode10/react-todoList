5fe79e6b2ab02cbfc65e0c4e2b4cb9b2f72c59b9
Mustaf <mustafdiiriye@hotmail.com>
2020-12-23 23:34:16 +0200

    contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,12 +7,19 @@ class Contact extends Component {
         return (
             <React.Fragment>
                 <h1 style={titleStyle}>Welcome to contact me.</h1>
-                <h2 style={contacInfo}>Contact Information.</h2>
+                <h2 style={contacInfo}>Contact Info.</h2>
                 <div style={contactStyle}>
                 <h3>Name:   Mustaf Ahmed</h3>
                 <h3>Phone:  00962786707948</h3>
                 <h3>Email:  mustafdirie1@gmail.com</h3>
                 </div>
+
+                <img src={Mustaf} />
+            
+                <p style={linkStyle}>
+                <a href="https://www.linkedin.com/in/mustaf-abtidon-135620179/">Visit my LinkedIn</a> And
+                < a href="https://github.com/mustafcode10?tab=repositories"> My Github</a>
+                </p>
                 
             </React.Fragment>
          
@@ -34,6 +41,10 @@ const contactStyle = {
     color: 'blue',
     textAlign: 'left',
     padding : '10px'
+}
+const linkStyle = {
+    textAlign: 'center',
+    color: 'white'
 }
 
 export default Contact;
